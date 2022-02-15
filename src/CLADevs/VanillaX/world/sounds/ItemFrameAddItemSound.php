458594e0737660121f35f11cce4ce0533f53c1dc
ItsRealNise <90197402+ItsRealNise@users.noreply.github.com>
2022-02-15 18:29:43 +0700

    Create ItemFrameAddItemSound.php

---
+++ src/CLADevs/VanillaX/world/sounds/ItemFrameAddItemSound.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+namespace CLADevs\VanillaX\world\sounds;
+
+
+
+use pocketmine\math\Vector3;
+
+use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
+
+use pocketmine\network\mcpe\protocol\types\LevelEvent;
+
+use pocketmine\world\sound\Sound;
+
+
+
+class ItemFrameAddItemSound implements Sound {
+
+
+
+    public function encode(Vector3 $pos): array
+
+    {
+
+        return [LevelSoundEventPacket::nonActorSound(LevelEvent::SOUND_ITEMFRAME_ADD_ITEM, $pos, false)];
+
+    }
+
+}
